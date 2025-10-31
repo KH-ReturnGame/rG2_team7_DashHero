503170f0d863d2c10f6bf50ffd3228a1a8283b03
qkdwnstn123 <qkdwnstn09@gmail.com>
2025-10-31 13:49:01 +0900

    투사체 발사함(멀리까지는안감,낳기만함)

--- Assets/Scripts/MonsterMove.cs
+++ Assets/Scripts/MonsterMove.cs
@@ -6,12 +6,23 @@ public class MonsterMove : MonoBehaviour
     public float attackRange;   // 변수값 정해주기
     private Transform player;
     private Animator anim;
-    
+
+    public GameObject projectile;
+
+
     void Start()
     {
         // Player 태그 붙은 오브젝트 찾기
         player = GameObject.FindWithTag("Player").transform;
+
         anim = GetComponent<Animator>();
+    }
+
+    public void Attack()
+    {
+        GameObject clonedProjectile = Instantiate(projectile, transform.position, Quaternion.identity);
+
+        Debug.Log("뿅");
     }
 
     void Update()
@@ -27,7 +38,7 @@ public class MonsterMove : MonoBehaviour
         }
         else
         {
-           // Debug.Log("공격 불가능");
+           //d Debug.Log("공격 불가능");
             anim.SetBool("isAttack", false);  // 대기 애니메이션
         }
     }
